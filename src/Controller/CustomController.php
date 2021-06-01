35f3d18873cf4b37a310089a76e1972eef6411a9
Oleh Holovin <o.holovin@kreuzfahrtberater.de>
2021-06-01 19:03:50 +0200

    controller

---
+++ src/Controller/CustomController.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Exception;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class CustomController
+{
+    /**
+     * @Route ("/numbers/get")
+     * @return Response
+     * @throws Exception
+     */
+    public function getNumbers(): Response
+    {
+        $numbers = [];
+        for ($i = 0; $i < 10; $i++) {
+            $numbers[] = random_int(0, 100);
+        }
+        if (count($numbers) > 0) {
+            return new Response(json_encode($numbers));
+        }
+
+        return new Response('No numbers were generated', 404);
+    }
+}
