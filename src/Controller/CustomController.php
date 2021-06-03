b0e28c0bddde36f547c2910b5ac46927809cc6bc
Oleh Holovin <o.holovin@kreuzfahrtberater.de>
2021-06-03 09:05:49 +0200

    templating

--- src/Controller/CustomController.php
+++ src/Controller/CustomController.php
@@ -5,10 +5,11 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Exception;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-final class CustomController
+final class CustomController extends AbstractController
 {
     /**
      * @Route ("/numbers/get")
@@ -23,7 +24,7 @@ final class CustomController
             $numbers[] = random_int(0, 100);
         }
         if (count($numbers) > 0) {
-            return new Response(json_encode($numbers));
+            return $this->render('custom/random_numbers.html.twig', ['response' => new Response(json_encode($numbers))]);
         }
 
         return new Response('No numbers were generated', 404);
